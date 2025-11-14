725ff5bb7385610a2966ea67c2032c4201440dad
devjubszx <julia9escola@gmail.com>
2025-11-14 09:58:29 -0300

    Adaptação DisplayCSV para leitura do ai_job_trends_dataset

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+CSV_FILE = "ai_job_trends_dataset.csv"
+
+def load_csv():
+    try:
+        df = pd.read_csv(CSV_FILE, sep=None, engine="python", encoding="utf-8")
+        return df
+    except Exception:
+        df = pd.read_csv(CSV_FILE, sep=None, engine="python", encoding="latin1")
+        return df
+
+@app.route("/")
 def index():
-    df = pd.read_csv("ai_job_trends_dataset.csv")
-    html_table = df.to_html(classes='table table-striped', index=False)
+    df = load_csv()
+    html_table = df.to_html(classes="table table-striped table-bordered", index=False)
     return render_template("index.html", table=html_table)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
